5df3323be24e7b07a5e69aee83e1c7df9ee27d11
hon3650 <105658717+hon3650@users.noreply.github.com>
2025-02-21 01:19:35 -0500

    menus
    menus started

---
+++ Assets/Scripts/UI/UI_Change_Scene_buttion.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Change_Scene_buttion : MonoBehaviour
+{
+    public int SceneNum;
+
+    public void change_Scene()
+    {
+        SceneManager.LoadScene(SceneNum);
+    }
+}
---
+++ Assets/Scripts/UI/UI_Quit_Buttion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Quit_Buttion : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
