0be208d3742aa5b3cf7dc152ae97e87cdc7fecfb
LoganArnott <larnott1@students.kennesaw.edu>
2025-02-23 01:15:49 -0500

    Updated Cameras

---
+++ Assets/Scripts/camera_follow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_follow : MonoBehaviour
+{
+    public GameObject player;
+    float oldPositionX;
+    float oldPositionY;
+    public string sceneName;
+
+    void Start()
+    {
+        oldPositionX = player.transform.position.x;
+        oldPositionY = player.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Level1
+        if(sceneName == "Level1" && player.transform.position.x >= 1.25 && player.transform.position.x <= 28.45) {
+            this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, -10f);
+        }
+
+        //Level2
+        if(sceneName == "Level2" && player.transform.position.y >= -2.45 && player.transform.position.y <= 34.5) {
+            this.transform.position = new Vector3(this.transform.position.x, player.transform.position.y + 2.45f, -10f);
+        }
+
+        // Level3
+        if(sceneName == "Level3" && player.transform.position.x >= 1.25 && player.transform.position.x <= 78.5) {
+            this.transform.position = new Vector3(player.transform.position.x, this.transform.position.y, -10f);
+        }
+    }
+}
--- Assets/Scripts/progress_bar.cs
+++ Assets/Scripts/progress_bar.cs
@@ -20,7 +20,7 @@ public class progress_bar : MonoBehaviour
     {
         // Level1
         if(sceneName == "Level1" && player.transform.position.x != oldPositionX && player.transform.position.x >= -8.95 && player.transform.position.x <= 36.6) {
-            this.transform.position = new Vector3(this.transform.position.x + ((player.transform.position.x - oldPositionX) / 3.2536f), player.transform.position.y + 2.45f, 0f);
+            this.transform.position = new Vector3(this.transform.position.x + ((player.transform.position.x - oldPositionX) / 3.2536f), this.transform.position.y, 0f);
             oldPositionX = player.transform.position.x;
         }
 
@@ -32,7 +32,7 @@ public class progress_bar : MonoBehaviour
 
         // Level3
         if(sceneName == "Level3" && player.transform.position.x != oldPositionX && player.transform.position.x >= -9.21 && player.transform.position.x <= 80.0) {
-            this.transform.position = new Vector3(this.transform.position.x + ((player.transform.position.x - oldPositionX) / 6.3721f), player.transform.position.y + 3.32f, 0f);
+            this.transform.position = new Vector3(this.transform.position.x + ((player.transform.position.x - oldPositionX) / 6.3721f), this.transform.position.y, 0f);
             oldPositionX = player.transform.position.x;
         }
     }
